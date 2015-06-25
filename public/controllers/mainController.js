e803fa75261b5698311fa49a8cab78e210271a2f
Naila Nur <nnur3@uwo.ca>
2015-06-25 15:05:10 -0400

    doodle help

--- public/controllers/mainController.js
+++ public/controllers/mainController.js
@@ -2,11 +2,28 @@ myapp.controller('mainController', function($scope, $http) {
 
     $scope.blogs = {};
 
-    $http.get('http://192.168.0.12:8000/blogtext').
+    $http.get('http://localhost:8000/blogtext').
     success(function(data, status, headers, config) {
         console.log(data)
 
         $scope.blogs = data;
+
+    }).
+    error(function(data, status, headers, config) {
+
+    });
+
+
+    $scope.image = {
+        uri: ''
+    };
+
+    $http.get('http://localhost:8000/blogimg').
+    success(function(data, status, headers, config) {
+
+        //console.log(data); //weird freakish satan symbols
+        // $scope.image = data;
+        $scope.image.uri = 'data:image/jpg;base64,' + data;
 
 
 
